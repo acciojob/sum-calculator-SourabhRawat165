b7749d3951dcd5203b89215b572796318c181b59
sourabh <sourabhrawat1560@gmail.com>
2025-10-16 20:40:55 +0530

    Commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,19 @@
-
-import React from "react";
+import React, { useState } from "react";
 import './../styles/App.css';
+import SumCalculator from "./SumCalculator";
 
 const App = () => {
+  const[sum, setSum] = useState(0)
   return (
     <div>
         {/* Do not remove the main div */}
+        <h1>Sum Calculator</h1>
+          <input onChange={(e) =>{
+            setSum((sum)+(Number(e.target.value)))
+          }} type="number"/>
+          <SumCalculator sum={sum} setSum={setSum}/>
     </div>
   )
 }
 
-export default App
+export default App
---
+++ src/components/sum-calculator.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const SumCalculator = ({sum, setSum}) => {
+  return (
+    <div>
+        <p>Sum:{sum}</p>
+    </div>
+  )
+}
+
+export default SumCalculator
+
+
+
